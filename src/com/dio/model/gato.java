e29ca638744189615e696233c97252191f9c20ea
Brendon da Silva Canto <92454258+Brendon-Lee@users.noreply.github.com>
2021-11-06 15:28:54 -0300

    Fist commit

---
+++ src/com/dio/model/gato.java
@@ -0,0 +1,16 @@
+package com.dio.model;
+
+public class gato {
+
+	private String nome;
+	private String cor;
+	private String idade;
+
+	public gato(String nome, String cor, integer idade) {
+		this.nome = nome;
+		this.cor  = cor;
+		this.idade= idade;
+		
+	}
+
+}
